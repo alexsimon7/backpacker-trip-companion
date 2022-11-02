9083a9c7e4caee77b9e5a468c3e6de9e43d5e9e8
Alex Simon <alexanderleesimon@gmail.com>
2022-11-02 14:58:10 -0400

    added additional products to have links pulled, notes re potential issues

--- dataScrapper/dataScrapper.js
+++ dataScrapper/dataScrapper.js
@@ -1,3 +1,14 @@
+/*
+TODO:
+  Issue: Products that Span More Than One Page
+    Solution: a. Hit Button to Display 90 Products, b. Hit Bottom Button to Go to Next Page If Exists
+  Issue: Duplicate Code
+    Solution: Encapsulate into functions when program works
+
+Input: None.
+Output: None. Create JSON FILE with scraped products (including names, sku?, and weight)
+ */
+
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
@@ -47,7 +58,45 @@ async function dataScrapper() {
     return productArray.map(element => `https://www.rei.com${element.getAttribute('href')}`);
   });
 
-  productArray = productArray.concat(backpacks, tents);
+  await page.goto('https://www.rei.com/c/mens-sleeping-bags');
+  await delay(2500);
+
+  let mensBags = await page.$$eval('#search-results > ul > li > a:first-of-type', productArray => {
+    return productArray.map(element => `https://www.rei.com${element.getAttribute('href')}`);
+  });
+
+  await page.goto('https://www.rei.com/c/womens-sleeping-bags');
+  await delay(2500);
+
+  let womenBags = await page.$$eval('#search-results > ul > li > a:first-of-type', productArray => {
+    return productArray.map(element => `https://www.rei.com${element.getAttribute('href')}`);
+  });
+
+  await page.goto('https://www.rei.com/c/sleeping-pads');
+  await delay(2500);
+
+  let sleepingPads = await page.$$eval('#search-results > ul > li > a:first-of-type', productArray => {
+    return productArray.map(element => `https://www.rei.com${element.getAttribute('href')}`);
+  });
+
+  await page.goto('https://www.rei.com/c/stoves-and-grills');
+  await delay(2500);
+
+  let stoves = await page.$$eval('#search-results > ul > li > a:first-of-type', productArray => {
+    return productArray.map(element => `https://www.rei.com${element.getAttribute('href')}`);
+  });
+
+  await page.goto('https://www.rei.com/c/hammocks');
+  await delay(2500);
+
+  let hammocks = await page.$$eval('#search-results > ul > li > a:first-of-type', productArray => {
+    return productArray.map(element => `https://www.rei.com${element.getAttribute('href')}`);
+  });
+
+
+
+
+  productArray = productArray.concat(backpacks, tents, mensBags, womenBags, sleepingPads, stoves, hammocks);
 
   console.log(productArray);
 
